278b922c94bc8c64b0b54aff23ec07b8a370f748
Wijnand Warren <god@ansuz.nl>
2014-11-04 22:03:41 +0100

    #1 Using BitmapShader instead of PorterDuff.

--- library/main/src/nl/ansuz/android/maskedimageview/widget/MaskedImageView.java
+++ library/main/src/nl/ansuz/android/maskedimageview/widget/MaskedImageView.java
@@ -4,12 +4,10 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
+import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
@@ -18,6 +16,7 @@ import android.util.AttributeSet;
 import android.view.InflateException;
 import android.view.View;
 import android.view.ViewOutlineProvider;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import nl.ansuz.android.maskedimageview.R;
 
@@ -31,12 +30,8 @@ public class MaskedImageView extends ImageView {
     protected Drawable mMaskDrawable;
     protected Bitmap mMask;
 
-    protected BitmapFactory.Options mSourceOptions;
-
-    protected Paint mSourcePaint;
-    protected Paint mMaskPaint;
-
     protected boolean mIsOutlineCompatible;
+    private Paint mShaderPaint;
 
     /**
      * @see ImageView#ImageView(Context)
@@ -104,24 +99,18 @@ public class MaskedImageView extends ImageView {
      * Initializes this class when using a pre-Lollipop API version.
      */
     private void initializeOutlineLegacy() {
-        mSourceOptions = new BitmapFactory.Options();
-        mSourceOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        if (isDeviceSdkCompatible(Build.VERSION_CODES.HONEYCOMB)) {
-            mSourceOptions.inMutable = true;
-        }
-
-        mSourcePaint = new Paint();
-        mSourcePaint.setAntiAlias(true);
-        mSourcePaint.setFilterBitmap(true);
-        mSourcePaint.setDither(true);
-
-        mMaskPaint = new Paint();
-        mMaskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-
         if (isDeviceSdkCompatible(Build.VERSION_CODES.HONEYCOMB)) {
             // Avoid issues with hardware accelerated rendering, i.e. black background showing instead of alpha.
             setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         }
+    }
+
+    /**
+     * @param image {@link Bitmap} - The image to base the shader on.
+     * @return {@link BitmapShader} - A new BitampShader.
+     */
+    private BitmapShader getShader(Bitmap image) {
+        return new BitmapShader(image, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
     }
 
     /**
@@ -155,19 +144,44 @@ public class MaskedImageView extends ImageView {
     }
 
     /**
-     * Loads the image mask.
+     * Loads the image mask.<br/>
+     * <br/>
+     * Bitmaps are the only primitive with which you cannot use shaders
+     * unless the bitmap’s configuration is Bitmap.Config.ALPHA_8.
      */
     private void loadMask() {
-        Drawable drawable = getResources().getDrawable(mMaskResourceId);
-        mMask = drawableToBitmap(drawable);
+        if (mMaskDrawable instanceof BitmapDrawable) {
+            // Only masks from Bitmaps need some extra care.
+            Bitmap bitmap = drawableToBitmap(mMaskDrawable, Bitmap.Config.ARGB_8888);
+            mMask = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ALPHA_8);
+            Canvas canvas = new Canvas(mMask);
+            canvas.drawBitmap(bitmap, 0.0f, 0.0f, null);
+        } else {
+            // Regular Shapes are fine without the extra steps.
+            mMask = drawableToBitmap(mMaskDrawable, Bitmap.Config.ALPHA_8);;
+        }
+    }
+
+    /**
+     * Creates the shader that holds the source Bitmap.<br/>
+     * Used in case Outline isn't available.
+     */
+    private void createShaderPaint() {
+        Bitmap image = drawableToBitmap(getDrawable(), Bitmap.Config.ARGB_8888);
+        Shader shader = getShader(image);
+
+        mShaderPaint = new Paint();
+        mShaderPaint.setAntiAlias(true);
+        mShaderPaint.setShader(shader);
     }
 
     /**
      * Converts a {@link Drawable} to a {@link Bitmap}.
      * @param drawable {@link Drawable} - The {@link Drawable} to convert.
+     * @param config {@link Bitmap.Config} - Bitmap config to use.
      * @return {@link Bitmap} - The resulting {@link Bitmap}.
      */
-    private Bitmap drawableToBitmap(Drawable drawable) {
+    private Bitmap drawableToBitmap(Drawable drawable, Bitmap.Config config) {
         if (drawable == null) {
             return null;
         }
@@ -184,7 +198,7 @@ public class MaskedImageView extends ImageView {
             bitmapHeight = getHeight();
         }
 
-        Bitmap bitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, config);
         Canvas canvas = new Canvas(bitmap);
         canvas.drawARGB(0, 0, 0, 0);
         drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -194,20 +208,11 @@ public class MaskedImageView extends ImageView {
     }
 
     /**
-     * @return {@link Rect} - The bounds to draw the source bitmap, based on the mask size.
-     */
-    private Rect getSourceBoundRect() {
-        return new Rect(0, 0, mMask.getWidth(), mMask.getHeight());
-    }
-
-    /**
      * Draws the "Outlined" or "masked" image for pre-Lollipop devices.
      * @param canvas {@link Canvas} - The Canvas to paint on.
      */
     private void drawOutlineLegacy(Canvas canvas) {
-        Bitmap sourceBitmap = drawableToBitmap(getDrawable());
-
-        if (sourceBitmap == null || getWidth() <= 0 || getHeight() <= 0) {
+        if (getWidth() <= 0 || getHeight() <= 0) {
             // Bail out early when there is nothing to do.
             super.onDraw(canvas);
             return;
@@ -217,25 +222,28 @@ public class MaskedImageView extends ImageView {
             loadMask();
         }
 
-        Bitmap output;
-        if (sourceBitmap.isMutable()) {
-            output = sourceBitmap;
-        } else {
-            output = sourceBitmap.copy(Bitmap.Config.ARGB_8888, true);
-        }
+        canvas.drawBitmap(mMask, 0.0f, 0.0f, mShaderPaint);
+    }
 
-        if (isDeviceSdkCompatible(Build.VERSION_CODES.HONEYCOMB_MR1)) {
-            output.setHasAlpha(true);
-        }
+    /** {@inheritDoc} */
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
 
-        // Make sure the original bitmap doesn't exceed the mask size.
-        Rect sourceBounds = getSourceBoundRect();
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 
-        // Paint image.
-        canvas.drawBitmap(output, sourceBounds, sourceBounds, mSourcePaint);
+            /** {@inheritDoc} */
+            @Override
+            public void onGlobalLayout() {
+                createShaderPaint();
 
-        // Set mask.
-        canvas.drawBitmap(mMask, 0, 0, mMaskPaint);
+                if (isDeviceSdkCompatible(Build.VERSION_CODES.JELLY_BEAN)) {
+                    getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+            }
+        });
     }
 
     /** {@inheritDoc} */
@@ -247,4 +255,5 @@ public class MaskedImageView extends ImageView {
             drawOutlineLegacy(canvas);
         }
     }
+
 }
